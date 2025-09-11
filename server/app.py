1652478f627f71a1839878fc593ab7e93cdb1b49
vipinchahal847 <2vipinchahal@gmail.com>
2025-09-11 17:10:37 +0530

    Remove duplicate https://localhost:3000 from CORS origins

--- server/app.py
+++ server/app.py
@@ -28,7 +28,6 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:3001", 
         "https://localhost:3000",
-        "https://localhost:3000",
         "https://localhost:3001",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
