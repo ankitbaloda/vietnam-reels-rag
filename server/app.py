ad3aecdfd533233d22d0d010d7a6bcd96fba3769
vipinchahal847 <2vipinchahal@gmail.com>
2025-09-11 18:36:52 +0530

    Add HTTPS localhost to CORS origins

--- server/app.py
+++ server/app.py
@@ -31,6 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:3001", 
+        "https://localhost:3000",
         "https://localhost:3001",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
