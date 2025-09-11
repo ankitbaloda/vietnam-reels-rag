380711630acbc3abb51fac3b90f0013749e4b419
vipinchahal847 <2vipinchahal@gmail.com>
2025-09-11 16:29:44 +0530

    Fix CORS 403 error for GitHub Codespaces

--- server/app.py
+++ server/app.py
@@ -24,7 +24,8 @@ app.add_middleware(
         "https://localhost:3000",
         "https://localhost:3001",
         "http://127.0.0.1:3000",
-        "http://127.0.0.1:3001"
+        "http://127.0.0.1:3001",
+        "https://*.app.github.dev"
     ],
     allow_credentials=True,
     allow_methods=["*"],
